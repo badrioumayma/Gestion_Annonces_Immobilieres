583b5f8e006e084171f744771cdff9df64ba4f09
oumayma badri <badrioumayma9@gmail.com>
2025-04-12 13:10:06 +0100

    navbar&sidebar

--- AgenceImmobiliere/src/app/app.module.ts
+++ AgenceImmobiliere/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,

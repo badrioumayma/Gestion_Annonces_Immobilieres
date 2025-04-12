9c9903e4439e883eb25f0620052b1d82912d665c
oumayma badri <badrioumayma9@gmail.com>
2025-04-12 13:30:51 +0100

    Card

--- AgenceImmobiliere/src/app/card/card.component.ts
+++ AgenceImmobiliere/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
+  @Input() icon: string = 'bi bi-house'; // e.g., 'bi bi-house'
+  @Input() title: string = 'Total Properties'; // e.g., 'Total Properties'
+  @Input() value: string = '444'; // e.g., '462'
 }
